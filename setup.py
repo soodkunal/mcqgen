ff9bcca9b890905dd53b007e7a16a462efadbe29
KunalSood <ksood315@gmail.com>
2025-07-22 19:53:06 +0530

    file update

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
 # For downloading Local Packages in the environment
+from setuptools import setup, find_packages
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author= "Kunal Sood",
+    author_email="kunalsood315@gmail.com",
+    install_requires=[
+        'openai',
+        'langchain',
+        'streamlit',
+        'python-dotenv',
+        'PyPDF2',],
+    packages=find_packages()
+)
